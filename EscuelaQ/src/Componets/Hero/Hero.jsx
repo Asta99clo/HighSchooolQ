837c5ab07179512130000c3118c00e88c1349460
Asta99clo <fran22sanc@gmail.com>
2025-09-24 23:31:56 -0600

    Componente Hero

--- EscuelaQ/src/Componets/Hero/Hero.jsx
+++ EscuelaQ/src/Componets/Hero/Hero.jsx
@@ -1,10 +1,22 @@
-const Hero = () =>{
-    return(
-        <>
-            <p>Soy el hero</p>
-            <p className="padre"></p>
-            
-        </>
-    )
-}
+import "./Hero.scss";
+
+const Hero = () => {
+  return (
+    <div className="hero-contenedor">
+      <div className="hero-texto">
+        <h1>Bienvenido a HighSchoolQ</h1>
+        <p>
+          Un texto literario es una obra escrita u oral que prioriza la función 
+          estética del lenguaje, buscando crear una experiencia estética en el 
+          lector a través de la belleza y la creatividad, en lugar de tener un 
+          propósito práctico o informativo directo.
+        </p>
+      </div>
+      <div className="hero-boton">
+        <button>¡Inscribite!</button>
+      </div>
+    </div>
+  );
+};
+
 export default Hero;
