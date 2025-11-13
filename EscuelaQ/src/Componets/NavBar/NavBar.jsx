7d6c8dd3dc26f72223d2e350704fe5f56b0963e5
Asta99clo <fran22sanc@gmail.com>
2025-11-13 10:16:39 -0600

    limpieza de codigo 2 componentes Hero y Navbar

--- EscuelaQ/src/Componets/NavBar/NavBar.jsx
+++ EscuelaQ/src/Componets/NavBar/NavBar.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'; // <-- 1. ¡IMPORTANTE!
+import { useState } from 'react'; 
 import './Navbar.scss';
 import { useNavigate } from "react-router-dom";
-// (No necesitas importar la imagen del hero aquí)
-
-// import logo from '../../assets/img/logo.svg'; 
 
 const NavBar = () => {
-  // --- 2. EL ESTADO PARA EL TOGGLE ---
+
+  // estado del tooggle
   const [isDarkMode, setIsDarkMode] = useState(false);
   const navigate = useNavigate();
 
-  // --- 3. LA FUNCIÓN QUE CAMBIA EL ESTADO ---
+  // la funvion que cambia el estado dle toggle
   const handleToggle = () => {
     setIsDarkMode(!isDarkMode);
     // document.body.classList.toggle('dark-mode'); // (Lógica futura)
   };
 
   return (
-    // CAMBIO: Ahora es <header>
     <header className="main-header"> 
       <nav className="main-nav">
-
-        {/* --- ENLACES --- */}
         <ul className='nav-links'>
           <li>
             <a href="/" className="nav-logo-link">
                         <img 
                 src="../../public/HighSchoolQ2.svg" 
                 alt="Logo de HighSchoolQ" 
-                className="navbar-logo" // Le damos una clase
+                className="navbar-logo" 
               />
               <span>HighSchoolQ</span>
             </a>
@@ -55,22 +50,18 @@ const NavBar = () => {
           </li>
         </ul>
 
-        {/* --- ACCIONES --- */}
         <div className="nav-actions">
           <button className="btn-secondary" onClick={()  => navigate("/Login")}>
             Iniciar sesión
           </button>
           
-          {/* --- 4. EL NUEVO TOGGLE CON SU LÓGICA --- */}
           <button 
             className={`btn-toggle ${isDarkMode ? 'active' : ''}`} 
             id='switch'
             onClick={handleToggle} // Se activa al hacer clic
           >
-            {/* El círculo que se desliza */}
             <span className="toggle-slider"></span>
             
-            {/* Los iconos */}
             <span className="icon-sun">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor"  viewBox="0 0 16 16">
                 <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
